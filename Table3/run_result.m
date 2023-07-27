aa03896fcb172de8d8a0d51e694d6e69f69817df
Yeqing Zhou <yeqingzhou92@gmail.com>
2023-07-27 16:32:27 +0200

    Create run_result.m

---
+++ Table3/run_result.m
@@ -0,0 +1,17 @@
+% Run this file to generate results in Table 3
+
+% Parameters
+
+
+% Step 1: find the optimal price and order-up-to level for traditional selliong for each setting
+
+
+
+% Step 2: find the optimal discount for opaque selling
+
+
+
+
+% Step 3: solve the dynamic pricing problem and save the profit
+
+
