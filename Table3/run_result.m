fc639f535c161e202a275930613989fa3ab24cda
Yeqing Zhou <yeqingzhou92@gmail.com>
2023-07-29 00:19:31 +0200

    Add files via upload

--- Table3/run_result.m
+++ Table3/run_result.m
@@ -8,11 +8,11 @@ aves=[100, 98, 96, 94, 92, 90];
 K = 1000;
 MC=50;
 sc=0.2;
-stdevs=[20, 10];
+stdevs=[10, 20];
 ii=0;
 
 Parameters=zeros(12,2);
-for i=1:2
+for i=1:6
     for j=1:2
         ii=ii+1;
         Parameters(ii,:)=[aves(i), stdevs(j)];
@@ -29,8 +29,8 @@ for iii=1:12
     Profits1=best_profit
     c1 = best_c1
     c2 = best_c2
-    rho = c2/c1
-    fprintf('Fixed policy - p= %3.2\n', best_fp);
+ 
+    fprintf('Fixed policy - p= %3.2f \n', best_fp);
     
     % Step 2: find the optimal discount for opaque selling
     run('dp_opaque_asy_MNL_min.m');
@@ -42,7 +42,7 @@ for iii=1:12
 % Step 3: solve the dynamic pricing problem and save the profit
     run('dp_asy_MNL_tau.m');
     Profits3=-lambda*T_hk(1,1)
-    OutputTable(iii,:)=[ave, stdev, Profits1, Profits2, Profits3 , (Profits2-Profits1)/Profits1, (Profits3-Profits1)/Profits1, rho];
+    OutputTable(iii,:)=[ave2, stdev, Profits1, Profits2, Profits3 , (Profits2-Profits1)/Profits1, (Profits3-Profits1)/Profits1, best_rho];
     toc
 end
 
