6d2f6d62e0b02df623cd3fe730cb5f70a2c95e84
Yeqing Zhou <yeqingzhou92@gmail.com>
2023-07-28 10:34:11 +0200

    Add files via upload

---
+++ Table2/UNI/Copy_of_dp_UNI_tau.m
@@ -0,0 +1,84 @@
+tic
+h=1;
+K = 1000;
+lambda=50;
+c = 50;
+c1 = c;
+c2 = c;
+best_fp = 100;
+stdev = 20;
+ave = 100;
+tau=.5;
+h_k=100*zeros(c1,c2);
+h_kminus1=-1000*rand(c1,c2);
+T_hk=-1000*rand(c1,c2);
+P1=zeros(c1,c2);
+P2=zeros(c1,c2);
+k=0;
+MC = 50;
+
+sc=1;
+% 
+% 
+% tau=.5;
+% h_k=100*zeros(c,c);
+% h_kminus1=1000*rand(c,c);
+% T_hk=1000*rand(c,c);
+% P1=zeros(c,c);
+% P2=zeros(c,c);
+% k=0;
+
+%Value iteration
+a=ave - .5*stdev*sqrt(12);
+b=ave + .5*stdev*sqrt(12);
+
+fp=best_fp;
+while k<20 && max(max(abs(h_k-h_kminus1)))> .1 %|| max(max(abs(T_hk-tau*h_kminus1)))/min(min(abs(T_hk-tau*h_kminus1))+10^-5)> 10.0001 
+    
+    if mod(k,1)==0
+        k
+max(max(abs(T_hk-tau*h_kminus1)))/min(min(abs(T_hk-tau*h_kminus1))+10^-5)
+max(max(abs(h_k-h_kminus1)))
+    end
+    k=k+1;
+    for i=1:c
+        for j=1:i
+            bestT_hk=1000000;
+            for d=max(ceil(.7*fp/sc),ceil(a/sc)):(fp/sc)
+%                 fp=f*sc;
+                dp=d*sc;
+                pi_small=(2*(b-fp)*(dp-a)+(b-fp)^2)/(2*(b-a)^2);
+                pi_big=(2*(b-dp)*(fp-a)+(b-dp)^2- (fp-dp)^2)/(2*(b-a)^2);
+                
+                    if i<j && i>1
+                        T_hk(i,j)=(i+j)*h/lambda+ pi_small*(tau*h_k(i-1,j)-fp+MC)+ pi_big*(tau*h_k(i,j-1)-dp+MC)+(1-pi_small-pi_big)*tau*h_k(i,j);
+                    elseif i<j && i==1
+                        T_hk(i,j)=(i+j)*h/lambda+ pi_small*(tau*h_k(c,c)+K-fp+MC)+ pi_big*(tau*h_k(i,j-1)-dp+MC)+(1-pi_small-pi_big)*tau*h_k(i,j);
+                    elseif i>j && j>1
+                        T_hk(i,j)=(i+j)*h/lambda+ pi_big*(tau*h_k(i-1,j)-dp+MC)+ pi_small*(tau*h_k(i,j-1)-fp+MC)+(1-pi_small-pi_big)*tau*h_k(i,j);
+                    elseif i>j && j==1
+                        T_hk(i,j)=(i+j)*h/lambda+ pi_big*(tau*h_k(i-1,j)-dp+MC)+ pi_small*(tau*h_k(c,c)+K-fp+MC)+(1-pi_small-pi_big)*tau*h_k(i,j);
+                    elseif i==1 && j==1
+                        T_hk(i,j)=(i+j)*h/lambda+ pi_big*(tau*h_k(c,c)+K-dp+MC)+ pi_small*(tau*h_k(c,c)+K-fp+MC)+(1-pi_small-pi_big)*tau*h_k(i,j);
+                    else
+                        T_hk(i,j)=(i+j)*h/lambda+ pi_big*(tau*h_k(i-1,j)-dp+MC)+ pi_small*(tau*h_k(i,j-1)-fp+MC)+(1-pi_small-pi_big)*tau*h_k(i,j);
+                    end
+                if T_hk(i,j)<bestT_hk
+                    bestT_hk=T_hk(i,j);
+                    P1(i,j)=dp;
+                    P2(i,j)=fp;
+                    P1(j,i)=dp;
+                    P2(j,i)=fp;
+                end
+            end
+            T_hk(i,j)=bestT_hk;
+            T_hk(j,i)=bestT_hk;
+        end
+    end
+    h_kminus1=h_k;
+    h_k=(1-tau)*h_k+ T_hk-T_hk(1,1);
+end
+
+Profit=-lambda*T_hk(1,1)
+toc
+
